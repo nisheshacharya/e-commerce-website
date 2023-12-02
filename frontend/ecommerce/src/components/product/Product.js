aa00c41fe84dc3297a62984028bbbbf0a66ae1ec
nisheshacharya <116048513+nisheshacharya@users.noreply.github.com>
2023-12-02 01:13:54 -0600

    late night commit

--- frontend/ecommerce/src/components/product/Product.js
+++ frontend/ecommerce/src/components/product/Product.js
@@ -1,4 +1,4 @@
-import '../../styles/Style.css'
+import '../../styles/Style.css';
 import { useEffect, useState } from "react";
 import { getUserName } from "../../network/network";
 
@@ -10,16 +10,25 @@ export default function Product({ product }) {
 
   console.log(product);
 
-  useEffect(()=>{
-    product.reviews.map((review)=>(totalRating += review.rating))
-    console.log(totalRating)
-    setAverageRating(totalRating/product.reviews.length)
+  useEffect(() => {
+    product.reviews.map((review) => (totalRating += review.rating));
+    console.log(totalRating);
+    setAverageRating(totalRating / product.reviews.length);
     console.log("average rating", averageRating);
-  })
-  const toggleReview = ()=>{
+  });
+
+  const toggleReview = () => {
     setShowReview(!showReview);
-    console.log(showReview)
-  }
+    console.log(showReview);
+  };
+
+  const addToCart = ()=>{
+    const localCart = JSON.parse(localStorage.getItem("cart")) || [];
+    localCart.push(product)
+    localStorage.setItem("cart", JSON.stringify(localCart));
+    console.log("saved in Local Storage")
+    }
+  
 
   return (
     <div className="product-container">
@@ -27,23 +36,28 @@ export default function Product({ product }) {
         <h2>{product.name}</h2>
         <h3>Rating: {averageRating}</h3>
         <div>
-        {product.images.map(image=> (<img src="${image}" />))}
+          {product.images.map((image, index) => (
+            <img key={index} src={image} alt={`Product Image ${index + 1}`} />
+          ))}
         </div>
-        <button>Add to cart</button>
-        <button onClick={toggleReview}> {showReview ? "Hide Review" : "Show Review"}</button>
+        <button onClick={addToCart}>Add to cart</button>
+
+        <button onClick={toggleReview}>{showReview ? "Hide Review" : "Show Review"}</button>
       </div>
 
-      {showReview && <div className="review-container">
-        <h4> Reviews</h4>
-        <div className="review">
-        {
-        product.reviews.map((review) => (<div className="review-each" key={review._id}> 
-        <p>{review.text}</p>
-        <h5> - {review.name || "Unknown User"}</h5>
-        </div>))
-        }
+      {showReview && (
+        <div className="review-container">
+          <h4> Reviews</h4>
+          <div className="review">
+            {product.reviews.map((review) => (
+              <div className="review-each" key={review._id}>
+                <p>{review.text}</p>
+                <h5> - {review.name || "Unknown User"}</h5>
+              </div>
+            ))}
+          </div>
         </div>
-      </div> }
+      )}
     </div>
   );
 }
